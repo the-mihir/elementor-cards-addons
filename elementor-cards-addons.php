8b63e7513b400c4b9711f83d751c0c18360743a0
Mihir <mihirkantho@gmail.com>
2025-01-07 17:56:47 +0600

    adding plugin header

---
+++ elementor-cards-addons.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name:       Elementor Cards Addons
+ * Plugin URI:        https://github.com/the-mihir/
+ * Description:       This is addons for Elementor making cards more stylish and attractive
+ * Version:           1.0.0
+ * Author:            Mihir Das
+ * Author URI:        https://github.com/the-mihir
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       elementor-cards-addons
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
